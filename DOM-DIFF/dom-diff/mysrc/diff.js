ac58b7d5f9bc9d4ef94b4662364c138e12255f23
TakiToolu <1256876216@qq.com>
2020-09-27 14:28:16 +0800

    “通过深度优先遍历识别变化，生成补丁重新渲染到原页面；待优化：平级的替换、新增节点
    git commit -m “通过深度优先遍历识别变化，生成补丁重新渲染到原页面；待优化：平级的替换、新增节点

--- DOM-DIFF/dom-diff/mysrc/diff.js
+++ DOM-DIFF/dom-diff/mysrc/diff.js
@@ -6,46 +6,78 @@
  - 文本的变化：{type：’TEXT'，text：1}
  *
 * */
-
-
+const ATTRS='ATTRS';
+const TEXT='TEXT';
+const REMOVE='REMOVE';
+const REPLACE='REPLACE';
+let Index=0;
+//判断两个节点的差异并返回
 function diff(oldTree,newTree){
   let patches={};
-  let index=0;
-  //遍历 递归树 比较后的结果放到补丁中
-  walk(oldTree,newTree,index,patches);
+  // let index=0;
+  // 遍历 递归树 比较后的结果放到补丁中
+  patches=walk(oldTree,newTree,Index,patches);
   return patches;
 }
+
+//判断两个节点属性的差异并返回
 function diffAttr(oldAttrs,newAttrs) {
   let patch={};
-  //判断老的属性和新的属性的关系
   for (let key in oldAttrs){
     if(oldAttrs[key]!==newAttrs[key]){
       patch[key]=newAttrs[key];//有可能undefined
     }
   }
-  //new 新的中多的属性
-  for(let key in newAttrs){
-    //老节点中没有新节点的属性
-    if(!oldAttrs[key].hasOwnProperty(key)){
+  for(let key in newAttrs){//老节点中没有新节点的属性
+    if(!oldAttrs.hasOwnProperty(key)){
+      // console.log(key)
       patch[key]=newAttrs[key];
     }
   }
+
   return patch;
 }
-function walk(oldTree,newNode,index,patches) {
+
+//判断两个节点的孩子节点差异并返回
+function diffChildren(oldChildren,newChildren,patches) {
+  oldChildren.forEach((child,idx)=>{
+    //index每次传递给walk时，index时递增的，所有人都基于一个序号来实现
+    walk(child,newChildren[idx],++Index,patches)
+  })
+}
+//判断节点是否为文本字符串
+function isString(node) {
+  return Object.prototype.toString.call(node)==='[object String]';
+}
+
+//对比两个节点 叶子节点返回差异补丁 或 对树枝 的深度遍历 进入孩子节点的遍历，将补丁一直传递下去//若参数index被私有化到了walk作用域内
+function walk(oldNode,newNode,index,patches) {
   let currentPatch=[];
-  //比较属性是否有更改
-  if(oldTree.type===newNode.type){
-    let attrs=diffAttr(oldTree.props,newNode.props);
-    if(Object.keys(attrs).length>0){
-      currentPatch.push({type:'ATTRS',attrs})
+  if(!newNode){//删除了节点
+    currentPatch.push({type:REMOVE,Index})
+  }
+  if(isString(oldNode)&&isString(newNode)){ //文本节点
+
+    if(oldNode!==newNode){
+      currentPatch.push({type:TEXT,text:newNode});
     }
+  }else if(oldNode.type===newNode.type){ //比较属性是否有更改
+    let attrs=diffAttr(oldNode.props,newNode.props);//属性名的不同
+    if(Object.keys(attrs).length>0){
+      console.log(attrs)
+      currentPatch.push({type:ATTRS,attrs})
+    }
+    if(oldNode.children){//若有则遍历儿子
+      diffChildren(oldNode.children,newNode.children,patches);
+    }
+  }else{ //节点被替换
+    currentPatch.push({type:REPLACE,newNode:newNode});
   }
   if(currentPatch.length>0){
-    //当前元素确实有补丁，将元素和补丁对应起来 放到补丁包中
-    patches[index]=currentPatch;
-    console.log(patches);
+    //当前元素确实有补丁，将元素和补丁对应起来 放到补丁包中、
+    patches[Index]= currentPatch;
   }
+  return patches;
 }
 
 
